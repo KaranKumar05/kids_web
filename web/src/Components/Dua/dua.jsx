5ef3f2abe07aeb89e249c16b5a7eac411a85cbde
KARAN KUMAR <kkvmalhi@gmail.com>
2024-01-17 23:24:35 +0500

    deploy

--- web/src/Components/Dua/dua.jsx
+++ web/src/Components/Dua/dua.jsx
@@ -7,7 +7,6 @@ import React, { useEffect } from "react";
 import SectionTitle from "../SectionTitle/sectionTitle";
 
 import "./dua.css";
-import Subtitle from "../SectionSubTitle/subtitle";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
